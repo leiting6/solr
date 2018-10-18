b8a4b184b8e7803c54b22608075a00ec5a4c61a0
leiting6 <leiting6@users.noreply.github.com>
2018-10-18 17:49:26 +0800

    Update upconfig-lnc.sh

--- upconfig-lnc.sh
+++ upconfig-lnc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ~/solrconfig && git pull
-bash ~/solrcloud/bin/solr zk upconfig -z 192.168.2.102:6181,192.168.2.103:6181,192.168.2.104:6181 -n lnc -d ~/solrconfig/lnc && curl "http://192.168.2.102:8983/solr/admin/collections?action=RELOAD&name=lnc"
+bash ~/solrcloud/bin/solr zk upconfig -z 192.168.2.XX:2181,192.168.2.XX:2181,192.168.2.XX:2181 -n COLLECTION -d ~/solrconfig/lnc && curl "http://192.168.2.XX:8983/solr/admin/collections?action=RELOAD&name=COLLECTION"
